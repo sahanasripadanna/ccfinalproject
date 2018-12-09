d85202b620acc42275d955aa4b73d14551a463d9
sahanasripadanna <sripadanna.sahana@gmail.com>
2018-12-09 12:53:34 -0500

    added image files + animation working

--- final/empty.js
+++ final/empty.js
@@ -1,13 +1,40 @@
-
+var tree;
+var forest = [];
+var inj;
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(windowWidth, windowHeight);
   print("help");
+  tree = createSprite(windowWidth/2, windowHeight/2);
+  tree.addAnimation('normal', 'finals/windchime001.png', 'finals/windchime001.png');
+  tree.addAnimation('moving', 'finals/windchime001.png', 'finals/windchime011.png');
+  tree.scale = .1;
+  tree.debug = true;
+  for (var i = 1; i < 10; i++){
+    forest[i] = createSprite(100*i, 100);
+    forest[i].addAnimation('normal', 'finals/windchime001.png', 'finals/windchime001.png');
+    forest[i].addAnimation('moving', 'finals/windchime001.png', 'finals/windchime011.png');
+    forest[i].scale = .2;
+    forest[i].debug = true;
+  }
 }
 
 function draw() {
   background(255,255,255);
   drawSprites();
   fill(0);
-  ellipse(windowWidth/2, windowHeight/2, 50, 50);
+  //tree.changeAnimation('normal');
+}
+
+function mouseClicked(){
+    wind = createSprite(mouseX, mouseY);
+    wind.addAnimation('normal', 'finals/windy01.png', 'finals/windy03.png');
+    wind.velocity.x = 3;
+    wind.life = 30;
+    wind.debug = true;
+    if (tree.overlapPoint(wind)){
+      tree.changeAnimation('moving');
+      print("ha");
+    }
+    tree.changeAnimation('moving');
+
 }
