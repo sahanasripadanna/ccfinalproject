3696e56e6abed7b8690ab68807ca224dc129e76e
sahanasripadanna <sripadanna.sahana@gmail.com>
2018-12-09 20:50:44 -0500

    group is working

--- final/empty.js
+++ final/empty.js
@@ -1,25 +1,23 @@
 var tree;
-var forest = [];
-var inj;
+var forest;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  print("help");
-  tree = createSprite(windowWidth/2, windowHeight/2);
-  tree.addAnimation('normal', 'finals/windchime001.png', 'finals/windchime001.png');
-  tree.addAnimation('moving', 'finals/windchime001.png', 'finals/windchime011.png');
-  tree.scale = .1;
-  tree.debug = true;
-  for (var i = 1; i < 10; i++){
-    forest[i] = createSprite(100*i, 100);
-    forest[i].addAnimation('normal', 'finals/windchime001.png', 'finals/windchime001.png');
-    forest[i].addAnimation('moving', 'finals/windchime001.png', 'finals/windchime011.png');
-    forest[i].scale = .2;
-    forest[i].debug = true;
+  forest = new Group();
+  for (var i = 0; i < 9; i++){
+    for (var j = 0; j < 3; j++){
+      tree = createSprite(100+150*i, 150+ 150*j);
+      tree.addAnimation('normal', 'finals/windchime001.png', 'finals/windchime001.png');
+      tree.addAnimation('moving', 'finals/windchime001.png', 'finals/windchime011.png');
+      tree.scale = .15;
+      tree.debug = true;
+      forest.add(tree);
+    }
   }
 }
 
 function draw() {
-  background(255,255,255);
+  background(119, 238, 155);
   drawSprites();
   fill(0);
   //tree.changeAnimation('normal');
@@ -29,12 +27,17 @@ function mouseClicked(){
     wind = createSprite(mouseX, mouseY);
     wind.addAnimation('normal', 'finals/windy01.png', 'finals/windy03.png');
     wind.velocity.x = 3;
-    wind.life = 30;
-    wind.debug = true;
-    if (tree.overlapPoint(wind)){
-      tree.changeAnimation('moving');
-      print("ha");
-    }
-    tree.changeAnimation('moving');
 
+    wind.debug = true;
+    print("before overlap");
+    wind.overlapPixel(forest, explosion);
+    wind.life = 60;
+    tree.changeAnimation('moving');
+}
+
+
+function explosion(wind, forest) {
+  wind.velocity.x -= .5;
+  print("haha");
+  forest.changeAnimation('moving');
 }
