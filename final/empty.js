c9dba23c42bbb24fb7f3218e244bb41bc8f34399
sahanasripadanna <sripadanna.sahana@gmail.com>
2018-12-11 12:48:41 -0500

    added audio files

--- final/empty.js
+++ final/empty.js
@@ -1,8 +1,21 @@
-var tree;
+var tree, wind;
 var forest;
+// function preload() {
+//   soundFormats('mp3', 'ogg');
+//   amb = loadSound('audio/forest.mp3');
+//   breeze = loadSound('audio/breeze.mp3');
+//   breeze.setVolume(1);
+//   amb.setVolume(1);
+//
+// }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  wind = createSprite(mouseX, mouseY);
+  wind.addAnimation('normal', 'finals/windy01.png', 'finals/windy03.png');
+  wind.velocity.x = 3;
+  wind.scale = .5;
+  
   forest = new Group();
   for (var i = 0; i < 9; i++){
     for (var j = 0; j < 3; j++){
@@ -14,6 +27,8 @@ function setup() {
       forest.add(tree);
     }
   }
+
+
 }
 
 function draw() {
@@ -23,21 +38,30 @@ function draw() {
   //tree.changeAnimation('normal');
 }
 
-function mouseClicked(){
-    wind = createSprite(mouseX, mouseY);
-    wind.addAnimation('normal', 'finals/windy01.png', 'finals/windy03.png');
-    wind.velocity.x = 3;
-
-    wind.debug = true;
-    print("before overlap");
-    wind.overlapPixel(forest, explosion);
-    wind.life = 60;
-    tree.changeAnimation('moving');
-}
-
-
-function explosion(wind, forest) {
-  wind.velocity.x -= .5;
-  print("haha");
-  forest.changeAnimation('moving');
-}
+// function mouseClicked(){
+//   //  breeze.pause();
+//     wind = createSprite(mouseX, mouseY);
+//     wind.addAnimation('normal', 'finals/windy01.png', 'finals/windy03.png');
+//     wind.velocity.x = 3;
+//     wind.scale = .5;
+//
+// //    print("before overlap");
+//     print(wind.overlap(forest, explosion));
+//     wind.life = 80;
+//
+//     breeze.play(0, 1, .75, random(0,20), 1.2);
+// }
+//
+// function mousePressed(){
+//     forest.get(1).changeAnimation('moving');
+//
+// }
+//
+// function react(){
+//
+// }
+// function explosion(wind, forest) {
+//   wind.velocity.x -= .5;
+//   print("haha");
+//   forest.get().changeAnimation('moving');
+// }
